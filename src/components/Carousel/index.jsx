1eb919edb429cd22d9cb69e13d1af011c49e26b1
Думан Торбаев <tdz@grapheme.ru>
2021-07-21 16:08:49 +0300

    refactor carousel

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import scss from './Carousel.module.scss'
+import {Button} from "../UI/Button";
+
+export const Carousel = ({photos, currentIndex}) => {
+    const [activeIndex, setActiveIndex] = useState(currentIndex)
+
+    const goPrev = () => {
+        let index = activeIndex
+        const slidesLength = photos.length;
+
+        if (index < 1) {
+            index = slidesLength;
+        }
+
+        --index;
+
+        setActiveIndex(index)
+    }
+
+    const goNext = () => {
+        let index = activeIndex;
+        let slidesLength = photos.length - 1;
+
+        if (index === slidesLength) {
+            index = -1;
+        }
+
+        ++index;
+
+        setActiveIndex(index)
+    }
+
+    return (
+        <div className={scss.wrap}>
+            <ul className={scss.carousel}>
+                {photos.map(({id, url, title}, index) => (
+                    <li key={id} className={`${scss.slide} ${index === activeIndex ? `${scss['_active']}` : ''}`}>
+                        <img id={id} src={url} alt={title}/>
+                    </li>
+                ))}
+            </ul>
+
+            <div className={scss.btns}>
+                <Button onClick={goPrev}>Prev</Button>
+                <Button onClick={goNext}>Next</Button>
+            </div>
+        </div>
+    );
+};
--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import scss from './Photo.module.scss'
 import {useHistory, useParams} from 'react-router-dom'
 import {useActions} from "../../hooks/useActions";
@@ -6,85 +6,54 @@ import {useSelector} from "react-redux";
 import {takePhotos} from "../../store/photos/selector";
 import {Button} from "../UI/Button";
 import {Popup} from "../UI/Popup";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.scss';
+import {Carousel} from "../Carousel";
 
 export const Photo = () => {
-    const {id} = useParams()
-    const {photos, loading} = useSelector(takePhotos)
-    const {getPhotosById} = useActions()
-    const history = useHistory()
-    const [popupInfo, setPopupInfo] = useState({})
-    const swiperRef = useRef(null);
+        const {id} = useParams()
+        const {photos, loading} = useSelector(takePhotos)
+        const {getPhotosById} = useActions()
+        const history = useHistory()
+        const [popupInfo, setPopupInfo] = useState({})
 
-    useEffect(() => {
-        getPhotosById(id)
-    }, [id])
+        useEffect(() => {
+            getPhotosById(id)
+        }, [id])
 
-    const goBack = () => {
-        history.goBack()
-    }
-
-    const handlePopupInfo = (event) => {
-        const index = event.target.dataset.id
-        setPopupInfo({index: index, open: true})
-    }
-
-    const goNext = () => {
-        if (swiperRef.current && swiperRef.current.swiper) {
-            swiperRef.current.swiper.slideNext();
+        const goBack = () => {
+            history.goBack()
         }
-    };
 
-    const goPrev = () => {
-        if (swiperRef.current && swiperRef.current.swiper) {
-            swiperRef.current.swiper.slidePrev();
+        const handlePopupInfo = (index) => {
+            setPopupInfo({index, open: true})
         }
-    };
 
-    if (loading) {
-        return (<h1>Загрузка...</h1>)
-    }
+        if (loading) {
+            return (<h1>Загрузка...</h1>)
+        }
 
-    return (
-        <>
-            <div className={scss.header}>
-                <h1>Photos</h1>
-                <Button onClick={goBack}>Back</Button>
-            </div>
-            <div className={scss.grid}>
-                {photos.map(({id, url, title}, index) => (
-                    <div key={id} className={scss.item}>
-                        <img data-id={index} src={url} alt={title} onClick={handlePopupInfo}/>
-                    </div>
-                ))}
-            </div>
-
-            <div className={scss.popup}>
-                {
-                    popupInfo.open &&
-                    <Popup setPopupInfo={setPopupInfo}>
-                        <Swiper
-                            spaceBetween={50}
-                            slidesPerView={3}
-                            grabCursor={true}
-                            initialSlide={popupInfo.index}
-                            centeredSlides={true}
-                            ref={swiperRef}
-                        >
-                            {photos.map(({id, url, title}) => (
-                                <SwiperSlide key={id}>
-                                    <img id={id} src={url} alt={title}/>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-                        <div className={scss['btn-wrap']}>
-                            <Button onClick={goNext}>Next</Button>
-                            <Button onClick={goPrev}>Prev</Button>
+        return (
+            <>
+                <div className={scss.header}>
+                    <h1>Photos</h1>
+                    <Button onClick={goBack}>Back</Button>
+                </div>
+                <div className={scss.grid}>
+                    {photos.map(({id, url, title}, index) => (
+                        <div key={id} className={scss.item}>
+                            <img src={url} alt={title} onClick={() => handlePopupInfo(index)}/>
                         </div>
-                    </Popup>
-                }
-            </div>
-        </>
-    );
-};
+                    ))}
+                </div>
+
+                <div className={scss.popup}>
+                    {
+                        popupInfo.open &&
+                        <Popup setPopupInfo={setPopupInfo}>
+                            <Carousel photos={photos} currentIndex={popupInfo.index}/>
+                        </Popup>
+                    }
+                </div>
+            </>
+        );
+    }
+;
